47063e32c5531f67c1955cb5449b7575dd474f6a
Can Nahum <cannahum@outlook.com>
2021-06-26 22:09:07 -0700

    fixed params and env variable

--- main.go
+++ main.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-var appEnv = os.Getenv("appEnv")
+var appEnv = os.Getenv("APP_ENV")
 
 type MyEvent struct {
 	Name string `json:"name"`
 }
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s! You are in %s", name.Name, appEnv), nil
+func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (string, error) {
+	var param MyEvent
+	err := json.Unmarshal([]byte(request.Body), &param)
+	if err != nil {
+		return err.Error(), err
+	}
+	return fmt.Sprintf("Hello %s! You are in %s", param.Name, appEnv), nil
 }
 
 func main() {
