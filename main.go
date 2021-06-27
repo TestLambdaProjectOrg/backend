4afa05c4bf894fa6d05060fc0fb90d6ecedea4d0
Can Nahum <cannahum@outlook.com>
2021-06-26 19:00:56 -0700

    added more verbiage

--- main.go
+++ main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+var appEnv = os.Getenv("appEnv")
 
 type MyEvent struct {
 	Name string `json:"name"`
 }
 
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+	return fmt.Sprintf("Hello %s! You are in %s", name.Name, appEnv), nil
 }
 
 func main() {
